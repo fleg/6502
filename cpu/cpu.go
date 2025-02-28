f1b75dd0911667a50bdded58a07ede7e64d6f55e
fleg <flegmatik.bel@gmail.com>
2025-02-28 19:11:00 +0300

    refactor flags to sep file

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -2,17 +2,7 @@ package cpu
 
 import "fmt"
 
-type Flags uint8
-
 const (
-	flagCarry     Flags = 1 << 0
-	flagZero      Flags = 1 << 1
-	flagInterrupt Flags = 1 << 2
-	flagDecimal   Flags = 1 << 3
-	flagBreak     Flags = 1 << 4
-	flagOverflow  Flags = 1 << 6
-	flagNegative  Flags = 1 << 7
-
 	resetVector = 0xfffc
 	stackInit   = 0xfd
 )
@@ -79,26 +69,6 @@ func (cpu *CPU) nextOp() {
 	op.Do(cpu)
 
 	cpu.PC += op.Size
-}
-
-func (cpu *CPU) setFlag(mask Flags, value bool) {
-	if value {
-		cpu.PS = cpu.PS | mask
-	} else {
-		cpu.PS = cpu.PS & ^mask
-	}
-}
-
-func (cpu *CPU) getFlag(mask Flags) bool {
-	return cpu.PS&mask == 1
-}
-
-func (cpu *CPU) updateZeroFlag(val uint8) {
-	cpu.setFlag(flagZero, val == 0)
-}
-
-func (cpu *CPU) updateNegativeFlag(val uint8) {
-	cpu.setFlag(flagNegative, val&0x80 > 0)
 }
 
 func (cpu *CPU) getOperand(am AddressMode) uint8 {
---
+++ cpu/flags.go
@@ -0,0 +1,33 @@
+package cpu
+
+type Flags uint8
+
+const (
+	flagCarry     Flags = 1 << 0
+	flagZero      Flags = 1 << 1
+	flagInterrupt Flags = 1 << 2
+	flagDecimal   Flags = 1 << 3
+	flagBreak     Flags = 1 << 4
+	flagOverflow  Flags = 1 << 6
+	flagNegative  Flags = 1 << 7
+)
+
+func (cpu *CPU) setFlag(mask Flags, value bool) {
+	if value {
+		cpu.PS = cpu.PS | mask
+	} else {
+		cpu.PS = cpu.PS & ^mask
+	}
+}
+
+func (cpu *CPU) getFlag(mask Flags) bool {
+	return cpu.PS&mask == 1
+}
+
+func (cpu *CPU) updateZeroFlag(val uint8) {
+	cpu.setFlag(flagZero, val == 0)
+}
+
+func (cpu *CPU) updateNegativeFlag(val uint8) {
+	cpu.setFlag(flagNegative, val&0x80 > 0)
+}
